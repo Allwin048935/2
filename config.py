53881934706a2ffd88d35f3abf048dbcc1b0b7a3
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-07-29 22:12:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 
-TELEGRAM_TOKEN = 'your_telegram_bot_token'
-TELEGRAM_CHAT_ID = 'your_telegram_chat_id'
+TELEGRAM_TOKEN = '7426543281:AAHu4Qf_KT6sGBaqUJU5ccFKZroHKrVc9R0'
+TELEGRAM_CHAT_ID = '1385370555'
